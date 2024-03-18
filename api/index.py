93f410dded3716f1be72b6d5dd96007694e343a9
Tim <zfq@tuta.io>
2024-03-18 22:28:39 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from dsbix import DSBApi
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/fetch_entries')
+def fetch_entries():
+    username = '299761'
+    password = 'cicero2223'
+    dsb_client = DSBApi(username, password)
+    entries = dsb_client.fetch_entries()
+    return jsonify(entries)
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run()
